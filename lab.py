c4defbcbd37a01a780bdef8a25e678dc9565e1bf
MatthewASC20 <matthew.adam.dejesus@gmail.com>
2023-11-29 04:57:23 -0500

    Initial Commit

---
+++ lab.py
@@ -0,0 +1,307 @@
+"""
+6.101 Lab 12:
+LISP Interpreter Part 1
+"""
+
+#!/usr/bin/env python3
+
+import sys
+import doctest
+
+sys.setrecursionlimit(20_000)
+
+# NO ADDITIONAL IMPORTS!
+
+#############################
+# Scheme-related Exceptions #
+#############################
+
+
+class SchemeError(Exception):
+    """
+    A type of exception to be raised if there is an error with a Scheme
+    program.  Should never be raised directly; rather, subclasses should be
+    raised.
+    """
+
+    pass
+
+
+class SchemeSyntaxError(SchemeError):
+    """
+    Exception to be raised when trying to evaluate a malformed expression.
+    """
+
+    pass
+
+
+class SchemeNameError(SchemeError):
+    """
+    Exception to be raised when looking up a name that has not been defined.
+    """
+
+    pass
+
+
+class SchemeEvaluationError(SchemeError):
+    """
+    Exception to be raised if there is an error during evaluation other than a
+    SchemeNameError.
+    """
+
+    pass
+
+
+############################
+# Tokenization and Parsing #
+############################
+
+
+def number_or_symbol(value):
+    """
+    Helper function: given a string, convert it to an integer or a float if
+    possible; otherwise, return the string itself
+
+    >>> number_or_symbol('8')
+    8
+    >>> number_or_symbol('-5.32')
+    -5.32
+    >>> number_or_symbol('1.2.3.4')
+    '1.2.3.4'
+    >>> number_or_symbol('x')
+    'x'
+    """
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value
+
+
+def tokenize(source):
+    """
+    Splits an input string into meaningful tokens (left parens, right parens,
+    other whitespace-separated values).  Returns a list of strings.
+
+    Arguments:
+        source (str): a string containing the source code of a Scheme
+                      expression
+    """
+    seperated = source.replace("(", " ( ").replace(")", " ) ")
+    lines = seperated.splitlines() #splits string into a list of lines
+    tokens = []
+    for line in lines:
+        partitioned = line.partition(";") #partitions line in code and comments
+        tokens += partitioned[0].split() # adds only the code to the tokens
+    return tokens
+
+def formated_correctly(tokens):
+    """
+    Checks that tokens have:
+      an equal number of closing and opening parenthesis
+      starts and end with a parenthesis
+    """
+    if isinstance(tokens, list):
+        if len(tokens) == 1:
+            return tokens[0] not in ("(", ")")
+        else:
+            equal_num_of_parenthesis =  tokens.count("(") == tokens.count(")")
+            starts_ends_with_parthesis = tokens[0] == "(" and tokens[-1] == ")"
+            return equal_num_of_parenthesis and starts_ends_with_parthesis
+    return True
+
+def parse(tokens):
+    """
+    Parses a list of tokens, constructing a representation where:
+        * symbols are represented as Python strings
+        * numbers are represented as Python ints or floats
+        * S-expressions are represented as Python lists
+
+    Arguments:
+        tokens (list): a list of strings representing tokens
+    """
+    if not formated_correctly(tokens):
+        raise SchemeSyntaxError
+    def nested_parser(rest):
+        parsed = []
+        i = 0
+        while i < len(rest):
+            token = rest[i]
+            if token == ')':
+                return parsed, i
+            if token == '(' :
+                i += 1 # add one to skip '('
+                nests, added_index = nested_parser(rest[i:])
+                parsed.append(nests)
+                i += added_index
+            else: 
+                parsed.append(number_or_symbol(token))
+            i += 1
+        return parsed, i
+    return nested_parser(tokens)[0][0]
+
+######################
+# Built-in Functions #
+######################
+
+
+scheme_builtins = {
+    "+": sum,
+    "-": lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
+}
+
+
+##############
+# Evaluation #
+##############
+
+
+def evaluate(tree):
+    """
+    Evaluate the given syntax tree according to the rules of the Scheme
+    language.
+
+    Arguments:
+        tree (type varies): a fully parsed expression, as the output from the
+                            parse function
+    """
+    raise NotImplementedError
+
+
+########
+# REPL #
+########
+
+import os
+import re
+import sys
+import traceback
+from cmd import Cmd
+
+try:
+    import readline
+except:
+    readline = None
+
+
+def supports_color():
+    """
+    Returns True if the running system's terminal supports color, and False
+    otherwise.  Not guaranteed to work in all cases, but maybe in most?
+    """
+    plat = sys.platform
+    supported_platform = plat != "Pocket PC" and (
+        plat != "win32" or "ANSICON" in os.environ
+    )
+    # IDLE does not support colors
+    if "idlelib" in sys.modules:
+        return False
+    # isatty is not always implemented, #6223.
+    is_a_tty = hasattr(sys.stdout, "isatty") and sys.stdout.isatty()
+    if not supported_platform or not is_a_tty:
+        return False
+    return True
+
+
+class SchemeREPL(Cmd):
+    """
+    Class that implements a Read-Evaluate-Print Loop for our Scheme
+    interpreter.
+    """
+
+    history_file = os.path.join(os.path.expanduser("~"), ".6101_scheme_history")
+
+    if supports_color():
+        prompt = "\033[96min>\033[0m "
+        value_msg = "  out> \033[92m\033[1m%r\033[0m"
+        error_msg = "  \033[91mEXCEPTION!! %s\033[0m"
+    else:
+        prompt = "in> "
+        value_msg = "  out> %r"
+        error_msg = "  EXCEPTION!! %s"
+
+    keywords = {
+        "define", "lambda", "if", "equal?", "<", "<=", ">", ">=", "and", "or",
+        "del", "let", "set!", "+", "-", "*", "/", "#t", "#f", "not", "nil",
+        "cons", "list", "cat", "cdr", "list-ref", "length", "append", "begin",
+    }
+
+    def __init__(self, use_frames=False, verbose=False):
+        self.verbose = verbose
+        self.use_frames = use_frames
+        self.global_frame = None
+        Cmd.__init__(self)
+
+    def preloop(self):
+        if readline and os.path.isfile(self.history_file):
+            readline.read_history_file(self.history_file)
+
+    def postloop(self):
+        if readline:
+            readline.set_history_length(10_000)
+            readline.write_history_file(self.history_file)
+
+    def completedefault(self, text, line, begidx, endidx):
+        try:
+            bound_vars = set(self.global_frame)
+        except:
+            bound_vars = set()
+        return sorted(i for i in (self.keywords | bound_vars) if i.startswith(text))
+
+    def onecmd(self, line):
+        if line in {"EOF", "quit", "QUIT"}:
+            print()
+            print("bye bye!")
+            return True
+
+        elif not line.strip():
+            return False
+
+        try:
+            token_list = tokenize(line)
+            if self.verbose:
+                print("tokens>", token_list)
+            expression = parse(token_list)
+            if self.verbose:
+                print("expression>", expression)
+            if self.use_frames:
+                output, self.global_frame = result_and_frame(
+                    *(
+                        (expression, self.global_frame)
+                        if self.global_frame is not None
+                        else (expression,)
+                    )
+                )
+            else:
+                output = evaluate(expression)
+            print(self.value_msg % output)
+        except SchemeError as e:
+            if self.verbose:
+                traceback.print_tb(e.__traceback__)
+                print(self.error_msg.replace("%s", "%r") % e)
+            else:
+                print(self.error_msg % e)
+
+        return False
+
+    completenames = completedefault
+
+    def cmdloop(self, intro=None):
+        while True:
+            try:
+                Cmd.cmdloop(self, intro=None)
+                break
+            except KeyboardInterrupt:
+                print("^C")
+
+
+if __name__ == "__main__":
+    # code in this block will only be executed if lab.py is the main file being
+    # run (not when this module is imported)
+    msg = "\n\t['bare-name']\n"
+    tokens = tokenize(msg)
+    print(tokens)
+    print(parse(tokens))
+    SchemeREPL(use_frames=False, verbose=False).cmdloop()
